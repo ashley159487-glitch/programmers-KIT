3fe86d6bd78ae0c988a99c0a630eb357a2c5026e
ashley159487-glitch <ashley159487@gmail.com>
2026-01-20 23:31:05 +0900

    Update 수 조작하기 2.py

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -11,3 +11,8 @@ def solution(numLog):
         elif c == -10 :
             answer += 'a'
     return answer
+
+수 조작하기1 문제의 역버전인데...
+변수[i] - 변수 [k] 는 인덱스의 번호를 구하는 게 아니라
+각 인덱스의 해당 값끼리 연산 하는 거다.
+인덱스를 대입해서 풀어야 하므로 for문도 range와 len을 써서 변수의 길이만큼 반복해야 한다.

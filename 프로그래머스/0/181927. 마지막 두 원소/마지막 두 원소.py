b1f0f4e43cf500a99653624884bceb77fd2f0c56
ashley159487-glitch <ashley159487@gmail.com>
2026-01-20 23:25:38 +0900

    Update 마지막 두 원소.py

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -4,3 +4,7 @@ def solution(num_list):
     else :
         num_list.append(num_list[-1] * 2)
     return num_list
+
+append 뒤엔 ()가 붙는다. 까먹지 말자.
+마지막 인덱스를 묻는 문제니까 [-1]로 마지막 인덱스를 찾아주고
+[-2]로 그 전 인덱스만 찾으면 쉽게 풀린다.

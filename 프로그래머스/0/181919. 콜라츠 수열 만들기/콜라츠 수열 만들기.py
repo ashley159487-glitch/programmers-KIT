52296c1eadb2400719e2a9d1f82aa2c4306080be
ashley159487-glitch <ashley159487@gmail.com>
2026-01-22 00:23:00 +0900

    Update 콜라츠 수열 만들기.py

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -9,3 +9,8 @@ def solution(n):
             n = 3 * n + 1
             answer.append(n)
     return answer
+
+평소에 while 뒤에 True만 봐서 다른 식이 들어온 게 어색하다.
+물론 True를 쓰고도 만들 수 있다.
+그리고 처음에 초기값을 안넣어서 반복문 밖에 그냥 초기값을 넣고 반복을 시작했는데,
+반복문 안에다가 초기값을 넣고 해도 된다.

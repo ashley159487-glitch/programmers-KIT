3475931ad0e75c3e89ba19fbe807f0bc4d263f82
ashley159487-glitch <ashley159487@gmail.com>
2026-01-23 00:34:01 +0900

    Update 진료과별 총 예약 횟수 출력하기.sql

--- 프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
+++ 프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
@@ -4,3 +4,8 @@ FROM APPOINTMENT
 WHERE APNT_YMD LIKE '2022-05%'
 GROUP BY MCDP_CD
 ORDER BY COUNT(PT_NO) ASC, MCDP_CD ASC
+
+이번 거는 문제를 따라 그대로 썼더니 바로 됐는데...
+어떤 건 쉽고, 어떤 건 어렵고 SQL도 깊이가 생각보다 깊다.
+그래도 이제 LIKE나 COUNT같은 명령어들을 어디서 어떻게 쓰는지 어느정도 감은 잡은 거 같아
+기쁘다.

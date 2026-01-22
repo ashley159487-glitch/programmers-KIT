cc32f461bd1d364e03080641e75fe2126d0d9ea2
ashley159487-glitch <ashley159487@gmail.com>
2026-01-23 00:06:03 +0900

    Update 문자열 여러 번 뒤집기.py

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -4,3 +4,8 @@ def solution(my_string, queries):
     for s, e in queries :
         c[s:e + 1] = c[s:e + 1][::-1]
     return answer.join(c)
+
+슬라이싱을 많이 써본적이 없어서 이번 문제를 봤을 때
+각 인덱스의 위치를 바꾸려고만 했다..
+뒤집어라 = [::-1] 기억하자.
+어느 구간을 변경하거나 하는 문제는 슬라이싱을 쓴다.

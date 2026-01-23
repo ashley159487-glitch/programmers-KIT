2851f816a277b430b11458dedf42b1d6a935bc7a
ashley159487-glitch <ashley159487@gmail.com>
2026-01-23 23:02:20 +0900

    Update 부분 문자열 이어 붙여 문자열 만들기.py

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -5,4 +5,8 @@ def solution(my_strings, parts):
         k = q[parts[i][0]:parts[i][1] + 1]
         answer += k
     return answer
-       
+
+아직도!!! 문자열을 직접 꺼내야하는 문제인지, 그 길이를 꺼내야 하는 문제인지
+구분 못하면 어떻게 하냐!!!
+enumerate도 배워놓고 쳐 까먹고...
+문제의 의도를 잘 파악해도 처음 출발하는 방향이 달라진다.

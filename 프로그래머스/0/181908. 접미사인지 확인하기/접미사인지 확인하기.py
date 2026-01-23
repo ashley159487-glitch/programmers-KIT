f8427cd2b850fba135d3e7f3c7c7a2bbf2e2cb0a
ashley159487-glitch <ashley159487@gmail.com>
2026-01-24 00:06:12 +0900

    Update 접미사인지 확인하기.py

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -3,4 +3,17 @@ def solution(my_string, is_suffix):
         return 1
     else : 
         return 0
-    
+
+접미사인지 확인하는 함수 : endswith()
+접두사인지 확인하는 함수 : startswith()
+
+인덱스 슬라이싱으로도 하는 방법이 있다.
+
+def solution(my_string, is_suffix):
+    if my_string[-len(is_suffix):] == is_suffix :
+        return 1
+    else : 
+        return 0
+
+이렇게하면 위의 함수들을 몰라도
+답을 낼 수 있다.

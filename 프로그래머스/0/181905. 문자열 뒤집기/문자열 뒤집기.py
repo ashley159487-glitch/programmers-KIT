05c6c8e3564a94f46c4df51c2f79415d42a93f58
ashley159487-glitch <ashley159487@gmail.com>
2026-01-25 00:03:39 +0900

    Update 문자열 뒤집기.py

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,3 +1,8 @@
 def solution(my_string, s, e):
     sli = my_string[:s] + my_string[s:e + 1][::-1] + my_string[e + 1:]
     return sli
+
+문제를 보고 무조건 for문이나 if문을 적는 버릇을 고치니까
+오히려 좀 더 문제를 보는 시야가 넓어진 것 같다.
+이 문제는 단순히 slicing을 해서 이어붙이면 되는 문제이기에
+for문이나 if로 뭘 해보려 하지 않고 단순하게 잘라서 이어붙여보았다.

cf2bc041f773a49d6472e0bfa9ec43018225292c
ashley159487-glitch <ashley159487@gmail.com>
2026-01-25 20:58:47 +0900

    Update ROOT 아이템 구하기.sql

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -3,3 +3,7 @@ SELECT I.ITEM_ID, I.ITEM_NAME FROM ITEM_INFO I
 JOIN ITEM_TREE T ON I.ITEM_ID = T.ITEM_ID
 WHERE PARENT_ITEM_ID IS NULL
 ORDER BY I.ITEM_ID ASC
+
+이제 JOIN이 들어간 1, 2 레벨 문제는 거의 풀 수 있을 거 같다.
+딱히 어려운 건 없었고, ROOT아이템은 즉 부모아이템이 NULL값일 테니까
+그거만 찾는 쿼리를 만들면 된다.

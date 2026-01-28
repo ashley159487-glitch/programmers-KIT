9ecc8bc41538d14b2b2598b9a41ea82cd5b6ed60
ashley159487-glitch <ashley159487@gmail.com>
2026-01-29 01:27:23 +0900

    Update 이름에 el이 들어가는 동물 찾기.sql

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -2,3 +2,7 @@
 SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
 WHERE NAME LIKE '%EL%' AND ANIMAL_TYPE LIKE 'DOG'
 ORDER BY NAME ASC
+===================================================
+WHERE절에서 한 번에 두가지의 조건을 걸 수 있다.
+그리고 찾으려고 하는 값이 딱 하나로 떨어지는 조건이면
+LIKE대신 =를 쓸 수 있다.

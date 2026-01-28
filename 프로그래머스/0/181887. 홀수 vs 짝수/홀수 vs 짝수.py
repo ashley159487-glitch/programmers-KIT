ec6d2a2e90e028db691b2113e0895b401690bf7b
ashley159487-glitch <ashley159487@gmail.com>
2026-01-28 23:05:49 +0900

    Update 홀수 vs 짝수.py

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -9,6 +9,22 @@ def solution(num_list):
         return answer
     else :
         return answer2
+==============================================================
+처음에
+def solution(num_list):
+    answer = 0
+    answer2 = 0
+    for i in range(len(num_list)) :
+        if i % 2 == 0 :
+            answer += num_list[i]
+        else :
+            answer2 += num_list[i]
+            return max(answer, answer2)
+이렇게 만들고 테스트를 시도해보는데 계속 테스트가 실패하길래
+그냥 아예 처음부터 끝까지 다 써보자 하는 마음으로 각각 for문을 돌리다가
+return이 for문 안에 있어서 답이 틀리는 것이었다는 걸 알았다.
+그냥 계속 쭉 코드를 써서 for문이 2개 있는 코드로 답을 맞추긴 했는데
+들여쓰기가 어디에 어떻게 되어있는지 꼭 유념하면서 문제를 풀어야겠다.
     
         
     

edd8d074cb598caeba77d68ac22fa763536e7dbe
ashley159487-glitch <ashley159487@gmail.com>
2026-01-28 23:17:22 +0900

    Update 5명씩.py

--- 프로그래머스/0/181886. 5명씩/5명씩.py
+++ 프로그래머스/0/181886. 5명씩/5명씩.py
@@ -2,3 +2,13 @@ def solution(names):
     answer = names[::5]
     
     return answer
+=============================
+어이가 없을 정도로 쉬운 문제지만
+며칠 전의 나였으면 이걸
+def solution(names):
+    answer = []
+    for i in range(0, len(names), 5) :
+        answer.append(names[i])
+    return answer
+이런 식으로 for문을 써서 풀었을 거 같다.
+슬라이싱의 편리함이 다시끔 remind되는 문제였다.

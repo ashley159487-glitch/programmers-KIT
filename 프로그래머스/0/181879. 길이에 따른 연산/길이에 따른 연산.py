c9eafa8b20c7ee3ec3575210ea710aaf185ccc12
ashley159487-glitch <ashley159487@gmail.com>
2026-01-30 01:16:51 +0900

    Update 길이에 따른 연산.py

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -8,4 +8,6 @@ def solution(num_list):
         for i in num_list :
             answer *= i
     return answer
-    
+================================
+for문 안에서 변수를 재선언 하면 반복이 돌 때마다 변수값이 계속
+재선언되기 때문에 변수의 재선언은 반복문 밖에다 하는 것이 좋다.

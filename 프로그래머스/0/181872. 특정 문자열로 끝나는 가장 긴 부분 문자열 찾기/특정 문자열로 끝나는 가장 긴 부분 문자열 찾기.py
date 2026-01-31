502073ce1bd5f3a890727ba4b47e175efdbfbb81
ashley159487-glitch <ashley159487@gmail.com>
2026-01-31 23:38:01 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -0,0 +1,8 @@
+def solution(myString, pat):
+    for i in range(len(myString), 0, -1) :
+        if myString[:i].endswith(pat) :
+            return myString[:i]
+        
+        
+            
+    

c60eb3169621034453718f316e80a81fb1d84d10
ashley159487-glitch <ashley159487@gmail.com>
2026-02-01 00:07:46 +0900

    Update ad 제거하기.py

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -4,3 +4,8 @@ def solution(strArr):
         if "ad" not in i :
             answer.append(i)
     return answer
+================================
+복잡하게 2중for문으로 돌려가면서 지울 필요 없이
+not in으로 걸러내면 된다.
+이 때 not과 in의 위치를 다르게 하면 결과값도 달라지니
+위치에 주의하도록 하자.

f2ff2eb543fb8552161c8010674e21e3504538f3
ashley159487-glitch <ashley159487@gmail.com>
2026-01-31 23:19:58 +0900

    Update 배열에서 문자열 대소문자 변환하기.py

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
@@ -8,3 +8,6 @@ def solution(strArr):
             j = j.upper()
             answer.append(j)
     return answer
+========================================
+enumerate 함수로 인덱스와 값을 동시에 뽑아내서
+비교하면 쉽게 풀 수 있다.

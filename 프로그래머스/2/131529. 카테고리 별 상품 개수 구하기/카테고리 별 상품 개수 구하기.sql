e6ca62a6d906f2e6c8984514b16f9c5edb400acd
ashley159487-glitch <ashley159487@gmail.com>
2026-02-01 00:49:53 +0900

    Update 카테고리 별 상품 개수 구하기.sql

--- 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
@@ -2,3 +2,7 @@
 SELECT LEFT(PRODUCT_CODE, 2) AS CATEGORY, COUNT(*) AS PRODUCTS FROM PRODUCT
 GROUP BY CATEGORY
 ORDER BY CATEGORY ASC
+===============================================================================
+LEFT RIGHT 적어놨던 거 같은데 벌써 잊었나...
+어쨌든 LEFT(PRODUCT_CODE, 2) 이렇게 쓰면 맨 왼쪽의 두 글자만 출력해준다.
+그리고 GROUP BY로 나눠줘야 출력이 똑바로 된다. 안하면 하나만 이상하게 출력됨.

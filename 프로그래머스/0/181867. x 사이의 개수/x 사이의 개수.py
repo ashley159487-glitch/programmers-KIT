911a1be26b6db4cd62f350f0f317eda7f0f8ee8b
ashley159487-glitch <ashley159487@gmail.com>
2026-02-01 17:49:40 +0900

    Update x 사이의 개수.py

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -3,6 +3,10 @@ def solution(myString):
     for i in myString.split("x") :
         answer.append(len(i))
     return answer
+=======================================
+처음엔 split을 하고 각 문자열의 갯수를 어떻게 표현 할까를 고민했었다.
+len() 을 쓰면 각 문자열의 길이만큼 갯수로 표현되므로
+리스트 안에 수로 표현되는 걸 깨달았다.
         
     
         

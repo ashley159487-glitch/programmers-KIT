5d5c4e030b6f24858a85dcc8fc5803339a5f2233
ashley159487-glitch <ashley159487@gmail.com>
2026-02-01 18:21:08 +0900

    Update 한 해에 잡은 물고기 수 구하기.sql

--- 프로그래머스/1/298516. 한 해에 잡은 물고기 수 구하기/한 해에 잡은 물고기 수 구하기.sql
+++ 프로그래머스/1/298516. 한 해에 잡은 물고기 수 구하기/한 해에 잡은 물고기 수 구하기.sql
@@ -1,4 +1,5 @@
 -- 코드를 작성해주세요
 SELECT COUNT(*) AS FISH_COUNT FROM FISH_INFO
 WHERE YEAR(TIME) = 2021
-
+=================================================
+슬슬 1, 2레벨 문제들이 끝나간다. 다시 생활코딩의 MYSQL을 볼 때가 왔다.

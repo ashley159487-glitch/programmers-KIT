9ec8a5c02ea07c56a772d75894e3ebb228980ee0
ashley159487-glitch <ashley159487@gmail.com>
2026-02-01 17:50:59 +0900

    Update 문자열 잘라서 정렬하기.py

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -4,3 +4,6 @@ def solution(myString):
         if i != "" :
             answer.append(i)
     return answer
+==============================================
+split으로 자르고 그냥 append하니까 결과값에 빈칸이 가장 먼저 정렬이 된다.
+빈칸은 정렬기준에 충족되지 않으므로 빼고 리스트에 넣는다.

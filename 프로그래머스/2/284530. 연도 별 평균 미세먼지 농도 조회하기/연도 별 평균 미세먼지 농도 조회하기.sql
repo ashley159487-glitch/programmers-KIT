d24e09cd09049404d6cee3f0530581a59362e534
ashley159487-glitch <ashley159487@gmail.com>
2026-02-01 18:17:15 +0900

    Update 연도 별 평균 미세먼지 농도 조회하기.sql

--- 프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/연도 별 평균 미세먼지 농도 조회하기.sql
+++ 프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/연도 별 평균 미세먼지 농도 조회하기.sql
@@ -5,3 +5,7 @@ FROM AIR_POLLUTION
 WHERE LOCATION1 LIKE '%경기%' AND LOCATION2 LIKE '%수원%'
 GROUP BY YEAR
 ORDER BY YEAR ASC
+============================================================
+
+=의 경우 완벽하게 똑같은 값을 찾을 때 이용하고
+LIKE의 경우 찾고싶은 글자가 포함되어있나를 확인 할 때 사용한다.

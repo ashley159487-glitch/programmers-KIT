b827467d20903af545aa7f0c1aa58e36881e7796
ashley159487-glitch <ashley159487@gmail.com>
2026-02-02 16:24:19 +0900

    Update 카테고리 별 도서 판매량 집계하기.sql

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -4,3 +4,8 @@ JOIN BOOK_SALES S ON B.BOOK_ID = S.BOOK_ID
 WHERE S.SALES_DATE LIKE '2022-01%'
 GROUP BY B.CATEGORY
 ORDER BY B.CATEGORY ASC
+===============================================================
+JOIN 뒤에 따라오는 건 ON이다.
+카테고리 별로 합산해야 하므로 GROUP BY 에 CATEGORY가 와야한다.
+날짜를 설정 할 때 날짜는 보통 년도부터 시작하므로 '%2022-01%'이 아니라
+'2022-01%'로 설정하는 게 좋다.

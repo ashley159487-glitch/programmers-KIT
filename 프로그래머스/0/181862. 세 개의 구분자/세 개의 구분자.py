d65e46dd04b24d756378d1532fcef8b223f4a895
ashley159487-glitch <ashley159487@gmail.com>
2026-02-02 10:25:57 +0900

    Update 세 개의 구분자.py

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -5,3 +5,16 @@ def solution(myStr):
         if i != "" :
             answer.append(i)
     return answer if answer else ["EMPTY"]
+=============================================
+이렇게 re를 import해서 한번에 a, b, c를 기준으로 자르는 방법도 있고
+==============================================
+def solution(myStr):
+    answer = []
+    charList = ['a', 'b', 'c']
+    for i in charList :
+        myStr = myStr.replace(i, " ")
+    answer = myStr.split()
+    return answer if answer else ["EMPTY"]
+===============================================
+이렇게 a, b, c를 담은 리스트를 만들고 for문으로 돌려서 replace후
+split하는 방법도 있다.

6deae7d8e1e20e81dc1020e1e050b7feb034f64f
ashley159487-glitch <ashley159487@gmail.com>
2026-02-03 09:54:32 +0900

    Update 배열 비교하기.py

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -16,5 +16,23 @@ def solution(arr1, arr2):
         return 1
     else :
         return -1
+===================================
+sum() 함수를 사용하면 훨씬 쉽게 풀 수 있다.
+
+def solution(arr1, arr2):
+    if len(arr1) > len(arr2) :
+        return 1
+    elif len(arr1) < len(arr2) :
+        return -1
+
+    sum1 = sum(arr1)
+    sum2 = sum(arr2)
+    
+    if sum1 > sum2 :
+        return 1
+    elif sum1 < sum2 :
+        return -1
+    else :
+        return 0
     
     

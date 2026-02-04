0c5a73bd428c47617fdb7db54ba1bfd63c2ad8f6
ashley159487-glitch <ashley159487@gmail.com>
2026-02-04 14:10:00 +0900

    Update 0 떼기.py

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,2 +1,7 @@
 def solution(n_str):
     return n_str.lstrip("0")
+=============================
+문자열에서 왼쪽에 특정 문자들을 지우고싶다 = lstrip
+오른쪽의 특정 문자들을 지우고싶다 = rstrip
+양 쪽 모두 지우고싶다 = strip
+문자열 안의 모든 특정 문자를 지우고 싶다 = replace

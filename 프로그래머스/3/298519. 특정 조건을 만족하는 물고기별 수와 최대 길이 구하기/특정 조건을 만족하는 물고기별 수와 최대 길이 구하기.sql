86ebadc73970f060b758d8eb1ab2e40cada727ca
ashley159487-glitch <ashley159487@gmail.com>
2026-02-04 14:55:48 +0900

    Update 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -4,3 +4,6 @@ FISH_TYPE FROM FISH_INFO
 GROUP BY FISH_TYPE
 HAVING AVG(IFNULL(LENGTH, 10)) >= 33
 ORDER BY FISH_TYPE ASC
+============================================================
+이 문제의 경우 HAVING절에서 그룹화된 결과에 직접 필터링을 수행하므로
+서브쿼리를 쓰는 것이 아니라 HAVING절에서 필터링을 할 수 있어야 한다.

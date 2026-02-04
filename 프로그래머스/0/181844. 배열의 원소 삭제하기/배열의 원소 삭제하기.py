74c240e9db596365ed7dfb0ef746b3087a0be141
ashley159487-glitch <ashley159487@gmail.com>
2026-02-04 14:34:30 +0900

    Update 배열의 원소 삭제하기.py

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -5,3 +5,7 @@ def solution(arr, delete_list):
             answer.append(j)
     
     return answer
+===================================
+주어진 두 리스트의 길이가 다를 경우
+==, !=를 써서 비교할 수 없다.
+in을 써서 한 리스트의 안에 값이 들어있는지 확인하자.

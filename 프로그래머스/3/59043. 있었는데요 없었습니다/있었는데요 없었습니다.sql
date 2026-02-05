7bcc8ece59b20ca9cd19fa41f658af4488f878b4
ashley159487-glitch <ashley159487@gmail.com>
2026-02-05 22:32:23 +0900

    Update 있었는데요 없었습니다.sql

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -3,3 +3,6 @@ SELECT O.ANIMAL_ID, O.NAME FROM ANIMAL_OUTS O
 JOIN ANIMAL_INS I ON O.ANIMAL_ID = I.ANIMAL_ID
 WHERE O.DATETIME < I.DATETIME
 ORDER BY I.DATETIME ASC
+==================================================
+두 테이블 안에 같은 컬럼이 있다고 아무거나 막 묶어서 JOIN하면 데이터가 엉망으로 나온다.
+FK가 될만한 컬럼을 찾아 JOIN하는 것이 중요하다.

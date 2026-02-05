15dbe2d8e01c2dd851a585a524229728139a4bb4
ashley159487-glitch <ashley159487@gmail.com>
2026-02-05 18:08:23 +0900

    Update 꼬리 문자열.py

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -4,3 +4,6 @@ def solution(str_list, ex):
         if ex not in i :
             answer += i
     return answer
+=============================
+리스트 형식이므로 반복문으로 각 문자열을 빼내어
+ex가 있는 문자열 빼고 나머지를 문자열로 합치면 된다.

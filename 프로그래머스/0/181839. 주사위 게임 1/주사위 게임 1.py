b1d4c2a122ef500480c41a5dd5e43a344d6c5015
ashley159487-glitch <ashley159487@gmail.com>
2026-02-05 18:19:58 +0900

    Update 주사위 게임 1.py

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -7,3 +7,14 @@ def solution(a, b):
         return 2 * (a + b)
     else :
         return abs(a - b)
+======================================
+abs() 절대값을 반환해주는 함수
+
+def solution(a, b):
+    if a % 2 == 1 and b % 2 == 1 :
+        return a*a + b*b
+    elif a % 2 == 1 or b % 2 == 1 :
+        return 2 * (a + b)
+    else :
+        return abs(a - b)
+이렇게 or를 써서 두번째와 세번째 단을 합쳐서 쓸 수 있다.

cbd4fbcbfd5f32f6ed626dd7523426c19c644035
ashley159487-glitch <ashley159487@gmail.com>
2026-02-05 22:44:39 +0900

    Update 오랜 기간 보호한 동물（1）.sql

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -4,3 +4,6 @@ LEFT JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
 WHERE O.ANIMAL_ID IS NULL
 ORDER BY I.DATETIME ASC
 LIMIT 3
+======================================================
+ANIMAL_OUTS의 NULL값을 구해야 하므로 LEFT JOIN을 해야한다.
+IS NULL로 NULL값이 있는 값들만 추출 후 상위 셋만 출력하게 한다.

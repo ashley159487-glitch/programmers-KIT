1579180a7a6b1b550e90fc126ff3d2dec09149ac
ashley159487-glitch <ashley159487@gmail.com>
2026-02-06 23:32:52 +0900

    Update 이차원 배열 대각선 순회하기.py

--- 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
+++ 프로그래머스/0/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.py
@@ -5,3 +5,8 @@ def solution(board, k):
             if i + j <= k :
                 answer += board[i][j]
     return answer
+===========================================
+문제대로 코드를 만들면 되는데 주의할 점이 있다.
+두번째 for문의 range에 len(board)를 넣게 되면 런타임 오류나 실패가 뜬다.
+왜냐하면 len(board)의 길이와 len(board[i])의 길이가 다를 수 있기 때문이다.
+이 것만 유념하면 문제대로 코드를 만들면 바로 풀린다.

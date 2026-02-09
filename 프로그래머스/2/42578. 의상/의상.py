6fe64debfd454ab8c6fbc951135aacdbfdde29dc
ashley159487-glitch <ashley159487@gmail.com>
2026-02-09 14:11:27 +0900

    Update 의상.py

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -22,3 +22,19 @@ collections 라이브러리를 import 한 다음
 여기서 주의해야 할 점은 "코니는 하루에 최소 한 개의 의상은 입습니다." 이것이다.
 그러므로 모든 의상을 안 입는 방법을 -1 해줘야 하므로 구한 수에 - 1을 해주면
 결과값이 나온다.
+==========================================
+def solution(clothes):
+    answer = 1
+    kinds = []
+    for i, j in clothes :
+        kinds.append(j)
+    counts = {}
+    for i in kinds :
+        counts[i] = counts.get(i, 0) + 1
+    for i in counts.values() :
+        answer *= i + 1
+    return answer - 1
+===========================================
+이번 풀이는 collections 라이브러리를 사용하지 않고
+반복문으로 직접 리스트에서 딕셔너리로 변환시킨 후
+값들을 계산후 답을 도출하는 풀이이다.

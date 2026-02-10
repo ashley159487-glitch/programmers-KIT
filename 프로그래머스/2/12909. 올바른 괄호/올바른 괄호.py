855b325ca91ad8ef0d39f5b08a51fdf1c830ac2d
ashley159487-glitch <ashley159487@gmail.com>
2026-02-10 16:04:57 +0900

    [level 2] Title: 올바른 괄호, Time: 6.98 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,14 @@
+def solution(s):
+    cnt = 0
+    for i in s :
+        if i == '(' :
+            cnt += 1
+        else :
+            cnt -= 1
+        if cnt < 0 :
+            return False
+    if cnt < 0 or cnt > 0 :
+        return False
+    else :
+        return True
+    

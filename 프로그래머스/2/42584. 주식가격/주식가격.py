1ed6f287c3b26a7c8f680be82125a656e7fd38cf
ashley159487-glitch <ashley159487@gmail.com>
2026-02-11 15:31:49 +0900

    Update 주식가격.py

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,6 +1,6 @@
 def solution(prices):
     answer = []
-    cnt = 0
+    
     for i in range(len(prices)) :
         cnt = 0
         for j in range(i + 1, len(prices)) :
@@ -11,4 +11,8 @@ def solution(prices):
                 break
         answer.append(cnt)
     return answer
-
+==================================================
+2중for문으로 각 인덱스를 기준으로 삼아서 뒤의 값과 크기 비교를 해준다.
+주의할 점은 크기 비교를 할 때는 이미 1초가 지나간 것이므로 뒤의 값이 크기가 작아도 cnt는 +1해준다.
+두번째 for문에서 시작점을 잡을 때는 처음 for문의 i + 1을 해줘야 각 인덱스의 뒤의 값들과 비교가 가능하다.
+만약 1, len(prices)라고 써놓으면 1번인덱스부터 비교하게된다.

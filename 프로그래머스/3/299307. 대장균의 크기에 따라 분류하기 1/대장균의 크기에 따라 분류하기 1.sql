f1f5b4d7b0a13c1d0cb01966c4a79fa192093dd3
ashley159487-glitch <ashley159487@gmail.com>
2026-02-13 14:01:14 +0900

    Update 대장균의 크기에 따라 분류하기 1.sql

--- 프로그래머스/3/299307. 대장균의 크기에 따라 분류하기 1/대장균의 크기에 따라 분류하기 1.sql
+++ 프로그래머스/3/299307. 대장균의 크기에 따라 분류하기 1/대장균의 크기에 따라 분류하기 1.sql
@@ -6,3 +6,7 @@ ELSE 'HIGH'
 END AS SIZE
 FROM ECOLI_DATA
 ORDER BY ID ASC
+=============================================
+CASE WHEN에 익숙해져야 한다.
+CASE WHEN이란?
+컬럼 내에서 각각의 값을 원래의 결과물 말고 내가 원하는 기준에 맞춰서 새로운 값으로 바꿔주는 쿼리이다.

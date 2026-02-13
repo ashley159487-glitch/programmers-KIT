8676fff1aea2c659641f7bb6fdb1bf8cb65eddb5
ashley159487-glitch <ashley159487@gmail.com>
2026-02-13 14:45:04 +0900

    Update 대장균들의 자식의 수 구하기.sql

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -3,3 +3,7 @@ SELECT A.ID, COUNT(B.ID) AS CHILD_COUNT FROM ECOLI_DATA A
 LEFT JOIN ECOLI_DATA B ON A.ID = B.PARENT_ID
 GROUP BY A.ID
 ORDER BY A.ID ASC
+============================================================
+LEFT JOIN으로 자기 자신을 합쳐서 결과를 도출해내야 하는데 주의 할 점은
+데이터끼리 매칭을 A.ID = B.PARENT_ID 이렇게 시켜야 각 ID별 그 ID를 부모로 둔 ID를 매칭 시킬수 있다.
+JOIN후에 COUNT를 하면 자식이 없어 NULL인 값은 자동으로 카운트를 하지않아 0으로 출력된다.

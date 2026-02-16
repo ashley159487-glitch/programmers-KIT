bf313284a7c5759488bcd0f02c806355ceff70d0
ashley159487-glitch <ashley159487@gmail.com>
2026-02-16 16:54:52 +0900

    Update 업그레이드 할 수 없는 아이템 구하기.sql

--- 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -9,3 +9,10 @@ ORDER BY ITEM_ID DESC
 이번 문제의 경우 굳이 JOIN으로 합칠 필요 없이 ID가 PARENT ID 컬럼 안에
 있는지 확인 하고 없으면 출력해야 하는 문제이므로 굳이 JOIN으로 합치지 않고, 
 서브쿼리를 통해 컬럼 안에 없는 ID들만 출력시키면 된다.
+==============================================================
+SELECT I.ITEM_ID, I.ITEM_NAME, I.RARITY FROM ITEM_INFO I
+LEFT JOIN ITEM_TREE T ON I.ITEM_ID = T.PARENT_ITEM_ID
+WHERE T.PARENT_ITEM_ID IS NULL
+ORDER BY I.ITEM_ID DESC
+===============================================================
+이런식으로 LEFT JOIN으로 합쳐서 나타낼 수도 있다.

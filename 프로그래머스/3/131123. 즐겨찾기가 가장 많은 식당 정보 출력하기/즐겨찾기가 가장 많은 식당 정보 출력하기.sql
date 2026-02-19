6f3bb263c3bdd140369160d8d50c13e5ceca294b
ashley159487-glitch <ashley159487@gmail.com>
2026-02-19 19:19:58 +0900

    Update 즐겨찾기가 가장 많은 식당 정보 출력하기.sql

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -3,3 +3,6 @@ SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES FROM REST_INFO
 WHERE (FOOD_TYPE, FAVORITES) IN (SELECT FOOD_TYPE, MAX(FAVORITES) FROM REST_INFO
                                 GROUP BY FOOD_TYPE)
 ORDER BY FOOD_TYPE DESC
+====================================================================================
+즐겨찾기수가 가장 많은 식당의 음식 종류
+문제에서 이거만 보고 식당 이름을 썼는데 종류로 구분해야 하기 때문에 음식 종류로 서브쿼리를 만들어야한다.

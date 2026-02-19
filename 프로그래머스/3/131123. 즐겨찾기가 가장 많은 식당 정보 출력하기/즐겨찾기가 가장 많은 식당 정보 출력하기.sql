d326be03b69ec91b909aeb906538252dd9a695bd
ashley159487-glitch <ashley159487@gmail.com>
2026-02-19 19:18:48 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,14 +1,5 @@
 -- 코드를 입력하세요
 SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES FROM REST_INFO
-WHERE (FOOD_TYPE, FAVORITES) IN (SELECT FOOD_TYPE, MAX(FAVORITES) AS FAVORITES
-                                 FROM REST_INFO
+WHERE (FOOD_TYPE, FAVORITES) IN (SELECT FOOD_TYPE, MAX(FAVORITES) FROM REST_INFO
                                 GROUP BY FOOD_TYPE)
-
 ORDER BY FOOD_TYPE DESC
-==================================================================================
-SELECT FOOD_TYPE, REST_ID, REST_NAME, MAX(FAVORITES) AS FAVORITES FROM REST_INFO
-GROUP BY FOOD_TYPE
-ORDER BY FOOD_TYPE DESC
-이렇게 쓸 경우 FAVORITES의 최대값은 잘 갖고 오지만 그 최대값을 가진 식당의 이름은 가져오지 못하므로
-서브쿼리를 이용해 FAVORITES의 최대값을 찾아낸 후 메인 쿼리에서 그 최대값에 맞는
-식당이름을 매치시켜야 한다.
